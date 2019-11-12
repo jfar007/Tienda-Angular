2dd055c1557a0cf3a06f0b24af0eeb1232523c1b
jfar007 <46226351+jfar007@users.noreply.github.com>
2019-11-12 12:53:52 -0500

    Cambios para la version 2

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,6 @@
 // This file includes polyfills needed by Angular and is loaded before the app.
 // You can add your own extra polyfills to this file.
-import 'core-js/ses/symbol';
+import 'core-js/es/symbol';
 import 'core-js/es/object';
 import 'core-js/es/function';
 import 'core-js/es/parse-int';
